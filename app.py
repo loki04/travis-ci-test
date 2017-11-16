9dafd2a9cefbf052558922aa3069c2fbbcacf2ea
Gabor Loki <loki@inf.u-szeged.hu>
2017-11-16 13:43:00 +0100

    Finalize the app

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 import platform
 import psutil
 import os
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    out = "Basic APP deployed by Travis CI!<br />\n"
+def index():
+    p=""
+    m=""
+    s=""
     try:
-        out += platform.platform() + "<br />\n"
-        out += str(psutil.virtual_memory()) + "<br />\n"
-        f = os.statvfs(".")
-        out += str(f.f_frsize * f.f_bavail) + "<br />\n"
-    except e:
-        out += str(e)
-    return out
+        p = platform.platform()
+        v = str(psutil.virtual_memory())
+        fs = os.statvfs(".")
+        f = str(fs.f_frsize * fs.f_bavail)
+    except:
+        p = 'error'
+    return render_template('index.html', platform = p, virtmem = v, freespace = f)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    Bootstrap(app)
     app.run(host='0.0.0.0', port=port)
